2487a0f1ef824a999556f1f21667b58d9ed05634
ParkerTenBroeck <ParkerTenBroeck@users.noreply.github.com>
2021-10-23 18:16:11 +0000

    deploy: ea15ab756d8aa54c510b211df2ebe99d12e78ac9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dce16f66ddc7f2b9f821a497e1556b6c.js"
+  "/Actions-Test/precache-manifest.2a7ad2cccd66eb1bea02a112ad4571ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Actions-Test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
