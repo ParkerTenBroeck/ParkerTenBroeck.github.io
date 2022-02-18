0035dc0186301dc31836e4fd3ef0f2f28ceb93a8
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2022-02-18 15:05:25 -0500

    fixed warnings

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import { Scrollbars } from 'react-custom-scrollbars';
-import { SpringSystem, MathUtil } from 'rebound';
+import { SpringSystem } from 'rebound';
 
 
 const globalScroll = () =>{
